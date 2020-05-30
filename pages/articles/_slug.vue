0dffa12ee30d3889a82ebdb8603e7541cd00d96f
Chris Schweda <cschweda@gmail.com>
2020-05-30 09:33:46 -0500

    fix

--- pages/articles/_slug.vue
+++ pages/articles/_slug.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
+  <div v-if="!error">
     <v-container>
       {{ $route.params.slug }}
       <p>{{ article[0] }}</p>
       Rendered:
-      <br >
-      <nuxt-content :document="article[0]" />
+      <div>
+        <nuxt-content :document="article[0]" />
+      </div>
       <!-- <div v-else class="mt-12 text-center">
       <v-progress-circular :size="70" :width="7" color="purple" indeterminate />
       </div>-->
